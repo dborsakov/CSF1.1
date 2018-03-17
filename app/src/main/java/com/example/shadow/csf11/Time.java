a5a378edee1b088525ae5e335a329c7b7acd937c
dborsakov <33338832+dborsakov@users.noreply.github.com>
2018-03-17 16:28:56 +0300

    Обновил класс Time  при создании в конструктор передается 3 числа типа int (year, mounth, day)  это дата когда началась учеба  имеет 2 метода  getDayOfWeek() - возвращает номер дня в недели  getTypeOfweek() - возвращает тип недели 0-знаменатель, 1-числитель

--- app/src/main/java/com/example/shadow/csf11/Time.java
+++ app/src/main/java/com/example/shadow/csf11/Time.java
@@ -4,11 +4,65 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+
 /**
  * Created by Shadow on 15.03.2018.
+ * Update by Daniil on 17.03.2018.
  */
 
 public class Time {
+
+    private int numOfWeek, sNumOfWeek;
+    private int sDay, sMounth, sYear;
+    //private int chis;
+    private int dayOfWeek;
+    private int typeOfWeek;
+
+
+     Time(){
+
+     }
+     Time(int start_day, int start_mounth, int start_year){
+        this.sDay = start_day;
+        this.sMounth = start_mounth;
+        this.sYear = start_year;
+
+        Calendar c = new GregorianCalendar(this.sYear, this.sMounth, this.sDay) ;
+        this.sNumOfWeek = c.get(Calendar.WEEK_OF_YEAR);
+
+        int chis = this.sNumOfWeek%2;
+
+        Calendar c_now = new GregorianCalendar();
+
+        this.numOfWeek = c_now.get(Calendar.WEEK_OF_YEAR);
+        this.dayOfWeek = c_now.get(Calendar.DAY_OF_WEEK);
+
+        int a = this.numOfWeek%2;
+
+        if(a==chis)
+        {
+            typeOfWeek = 0;
+        }
+        else
+        {
+            typeOfWeek = 1;
+        }
+
+    }
+
+    int getDayOfWeek(){
+        return dayOfWeek;
+    }
+
+    int getTypeOfWeek(){
+        return typeOfWeek;
+    }
+
     public int main(){
         String datanow = ""; // считать дату
         String datastart = "12.03.2018";
@@ -33,6 +87,5 @@ public class Time {
 
         // если больше семи то сменить на числитель/знаменатель
     }
-
 
 }
